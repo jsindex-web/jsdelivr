67299e88b85e239b87febc73b4ade72230aae40a
jsindex-web <bigdog8879@gmail.com>
2026-03-24 18:15:09 +0700

    Update loadersrc.js

--- loadersrc.js
+++ loadersrc.js
@@ -8,7 +8,28 @@
       document.head.appendChild(s);
     });
   }
+  function loadScript(src){
+  return new Promise((resolve, reject)=>{
+    var s = document.createElement('script');
+    s.src = src + '?v=' + Date.now();
 
+    const timeout = setTimeout(()=>{
+      reject("Timeout loading: " + src);
+    }, 10000);
+
+    s.onload = () => {
+      clearTimeout(timeout);
+      resolve();
+    };
+
+    s.onerror = () => {
+      clearTimeout(timeout);
+      reject("Failed loading: " + src);
+    };
+
+    document.head.appendChild(s);
+  });
+}
   Promise.all([
     loadScript('https://cdn.jsdelivr.net/gh/jsindex-web/jsdelivr@main/data-loader.js'),
     loadScript('https://cdn.jsdelivr.net/gh/jsindex-web/jsdelivr@main/jgnmss.js')
